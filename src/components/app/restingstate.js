efcc8d20d181d1ec529f3aa1bca04a432c53daef
Ronit-Desai <ronitdesai97@gmail.com>
2021-11-28 19:19:28 -0400

    Resting State 0.1

--- src/components/app/restingstate.js
+++ src/components/app/restingstate.js
@@ -1,41 +1,87 @@
 import React from "react";
+import moment from "moment";
+import HelperUtil from "../../util/HelperUtil";
+import Results from "./results";
 import { ButtonGroup, Dropdown, ToggleButton } from "react-bootstrap";
 import ReactCountdownClock from "react-countdown-clock";
+
+let headers = [
+  { label: "TimeStamp", key: "timestamp" },
+  { label: "Index", key: "index" },
+  { label: "ch_0", key: "ch_0" },
+  { label: "ch_1", key: "ch_1" },
+  { label: "ch_2", key: "ch_2" },
+  { label: "ch_3", key: "ch_3" },
+  { label: "participant_id", key: "participant_id" },
+];
+
+let headersWithAux = [
+  { label: "TimeStamp", key: "timestamp" },
+  { label: "Index", key: "index" },
+  { label: "ch_0", key: "ch_0" },
+  { label: "ch_1", key: "ch_1" },
+  { label: "ch_2", key: "ch_2" },
+  { label: "ch_3", key: "ch_3" },
+  { label: "ch_4", key: "ch_4" },
+  { label: "participant_id", key: "participant_id" },
+];
 
 class RestingState extends React.Component {
   constructor(props) {
     super();
 
     this.state = {
+      participantId: props.participantId,
+      isAuxConnected: props.isAuxConnected,
       experimentStarted: false,
       experimentCompleted: false,
       timeInterval: "1",
       eyeOption: "Open",
+      canCloseTab: true,
     };
 
     this.client = props.museClient;
     this.readings = [];
-    this.n170Readings = [];
+    this.restingstateReadings = [];
     this.processedData = [];
   }
 
   componentDidMount() {
-    // this.startN170();
-    // this.setState({ canCloseTab: false });
-    // window.addEventListener("beforeunload", this.unLoadEvent);
+    this.startRestingState();
+    this.setState({ canCloseTab: false });
+    window.addEventListener("beforeunload", this.unLoadEvent);
   }
 
   componentWillUnmount() {
-    //     if (!this.state.canCloseTab) {
-    //       window.removeEventListener("beforeunload", this.unLoadEvent);
-    //     }
+    if (!this.state.canCloseTab) {
+      window.removeEventListener("beforeunload", this.unLoadEvent);
+    }
   }
 
   render() {
     if (this.state.experimentCompleted) {
       return (
         <div className="App">
-          <p>Experiment completed</p>
+          <Results
+            rawReadings={this.readings}
+            processedDataHeaders={
+              this.state.isAuxConnected ? headersWithAux : headers
+            }
+            processedData={this.processedData}
+            processedFileName={
+              this.state.participantId +
+              "_restingstate_eeg_" +
+              moment(new Date()).format("YYYYMMDDHHmmss") +
+              ".csv"
+            }
+            experimentReadings={this.restingstateReadings}
+            experimentFileName={
+              this.state.participantId +
+              "_restingstate_readings_" +
+              moment(new Date()).format("YYYYMMDDHHmmss") +
+              ".csv"
+            }
+          ></Results>
         </div>
       );
     }
@@ -157,6 +203,13 @@ class RestingState extends React.Component {
     }
   }
 
+  startRestingState = async () => {
+    await this.client.start();
+    this.client.eegReadings.subscribe((reading) => {
+      this.readings.push(reading);
+    });
+  };
+
   startExperiment = () => {
     setTimeout(() => {
       this.stopExperiment();
@@ -164,7 +217,39 @@ class RestingState extends React.Component {
   };
 
   stopExperiment = () => {
+    this.client.disconnect();
+    this.processedData = HelperUtil.cleanData(
+      this.readings,
+      "restingstate",
+      this.state.participantId
+    );
     this.setState({ experimentCompleted: true });
+
+    let jsonBody = {};
+    jsonBody.participantId = this.state.participantId;
+    jsonBody.experiment = "restingstate";
+    jsonBody.isAuxConnected = this.state.isAuxConnected;
+    jsonBody.eegResult = this.processedData;
+    jsonBody.restingstateReadings = this.restingstateReadings;
+    jsonBody = JSON.stringify(jsonBody);
+
+    const postData = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: jsonBody,
+    };
+
+    fetch("https://museapp-backend-test1.azurewebsites.net/saveData", postData)
+      .then((response) => {
+        console.log("Data sent to server");
+        window.removeEventListener("beforeunload", this.unLoadEvent);
+        this.setState({ canCloseTab: true });
+      })
+      .catch((error) => {
+        console.log("Failure while sending data to server :(");
+        window.removeEventListener("beforeunload", this.unLoadEvent);
+        this.setState({ canCloseTab: true });
+      });
   };
 
   unLoadEvent = (e) => {
